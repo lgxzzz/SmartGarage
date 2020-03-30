653e6a9e783dd1097b4f76591301e2ee069a232e
lgxzzz <396597748@qq.com>
2020-03-30 08:04:53 +0800

    add ui

---
+++ app/src/main/java/com/smartgarage/fragment/FastFragment.java
@@ -0,0 +1,36 @@
+package com.smartgarage.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.smartgarage.R;
+
+
+public class FastFragment extends Fragment{
+
+
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_garage, container, false);
+        initView();
+        initData();
+        return view;
+    }
+
+    public static FastFragment getInstance() {
+        return new FastFragment();
+    }
+
+    public void initView(){};
+
+    public void initData(){
+
+    };
+}
---
+++ app/src/main/java/com/smartgarage/fragment/NaviFragment.java
@@ -0,0 +1,36 @@
+package com.smartgarage.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.smartgarage.R;
+
+
+public class NaviFragment extends Fragment{
+
+
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_garage, container, false);
+        initView();
+        initData();
+        return view;
+    }
+
+    public static NaviFragment getInstance() {
+        return new NaviFragment();
+    }
+
+    public void initView(){};
+
+    public void initData(){
+
+    };
+}
